758539bec65abefbb482464e167c8069efec497c
John Lee <johnqwerty.lee1@gmail.com>
2024-02-21 18:02:31 -0800

    Added alias for various programs

---
+++ configs/main/aliases.sh
@@ -0,0 +1,4 @@
+alias obsidian='"/opt/Obsidian/obsidian" > /dev/null 2>&1 & '
+alias dbeaver='"/usr/share/dbeaver-ce/dbeaver" > /dev/null 2>&1 & '
+alias filezilla='/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=filezilla org.filezillaproject.Filezilla > /dev/null 2>&1 &'
+
