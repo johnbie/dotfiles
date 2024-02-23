398f75feae16b12cfc1ac5de64eb94bdb55925c1
John Lee <johnqwerty.lee1@gmail.com>
2024-02-23 09:15:30 -0800

    Added more aliases

--- configs/notes/aliases.sh
+++ configs/notes/aliases.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 alias notes='cd ~/notes'
 alias second-brain='cd ~/notes/second-brain'
-
+alias books='cd ~/notes/books'
+alias music-sheets='cd ~/notes/music-sheets'
+alias vansoftworks-technical-wiki='cd ~/notes/vansoftworks-technical-wiki'
