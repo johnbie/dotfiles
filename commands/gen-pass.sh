4aca89fb939cc50483db67d94bd68ebebfa4241c
John Lee <johnqwerty.lee1@gmail.com>
2024-03-04 12:44:06 -0800

    Added password generator

---
+++ commands/gen-pass.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Usage Guide
+# 
+# Pass the length of the password you want (i.e. `./gen-pass 12`)
+
+password_length=$1
+head /dev/urandom | tr -cd '[:alnum:]!@#$%^&*.?;:~' | cut -c 1-$password_length
