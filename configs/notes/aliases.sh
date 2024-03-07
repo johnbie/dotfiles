1409766fe3b5598cf239b9b79b4400cc4383ea90
John Lee <johnqwerty.lee1@gmail.com>
2024-03-06 16:08:23 -0800

    Removed notes folder

--- configs/notes/aliases.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-alias notes='cd ~/notes'
-alias second-brain='cd ~/notes/second-brain'
-alias books='cd ~/notes/books'
-alias music-sheets='cd ~/notes/music-sheets'
-alias vansoftworks-technical-wiki='cd ~/notes/vansoftworks-technical-wiki'
