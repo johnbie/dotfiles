b904a6c8fe4928c2f9863e3399d7ea80fafd238c
John Lee <johnqwerty.lee1@gmail.com>
2024-03-11 11:02:51 -0700

    Added alias for chromium browser opening

---
+++ commands/chrome.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Usage Guide
+#
+# Pass the profile number and url (optional):
+# ```
+# ./chrome.sh 1 https://google.com
+# ```
+
+profile_number=$1
+url=''
+
+if [ $# -gt 1 ]; then
+	url=$2
+fi
+
+eval "chromium --args --profile-directory='Profile $profile_number' $url"
