5bbb33705f787c356a2f1b5be26db87470ab307e
John Lee <johnqwerty.lee1@gmail.com>
2024-03-18 12:09:41 -0700

    Renamed config.sh to configs.sh

--- autorun/autosource_configs.sh
+++ autorun/autosource_configs.sh
@@ -2,8 +2,8 @@ configs_dir="$HOME/dotfiles/configs"
 if [ -d "$configs_dir" ]; then
     # Loop through each sub-folder
     for folder in "$configs_dir"/*; do
-        if [ -f "$folder/config.sh" ]; then
-            source "$folder/config.sh"
+        if [ -f "$folder/configs.sh" ]; then
+            source "$folder/configs.sh"
         fi
     done
 fi
