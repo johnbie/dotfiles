f9daf9005ca964e938944f11f74804fac316fe47
John Lee <johnqwerty.lee1@gmail.com>
2024-03-22 18:56:37 -0700

    Added alias for editing nvim settings

--- configs/main/aliases.sh
+++ configs/main/aliases.sh
@@ -6,5 +6,8 @@ alias dotfiles='cd $HOME/dotfiles'
 alias helper='cd $HOME/dev/helper-scripts'
 alias shell-sandbox='cd $HOME/dev/shell-sandbox'
 
+# vim edit a file
+alias vimconfig="vim $HOME/.config/nvim/init.vim"
+
 # reboot as windows (main desktop only!)
 alias reboot-windows='sudo grub-reboot 2 && reboot'
