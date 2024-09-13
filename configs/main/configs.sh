76fafc4fd03a358da25cf7ebb6f81eb167c82c1b
John Lee <johnqwerty.lee1@gmail.com>
2024-09-13 10:17:18 -0700

    Hard-mapped nvm sourcing

--- configs/main/configs.sh
+++ configs/main/configs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # nvm
-export NVM_DIR="$HOME/.config/nvm"
+export NVM_DIR="$HOME/dotfiles/.config/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
