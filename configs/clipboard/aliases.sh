72d7d52c264618ebcc552dff0eec8d52e4962cad
John Lee <johnqwerty.lee1@gmail.com>
2025-03-28 14:45:56 -0700

    Updated alias name for parsing json string

--- configs/clipboard/aliases.sh
+++ configs/clipboard/aliases.sh
@@ -1,4 +1,4 @@
 alias xclipi='xclip -selection c'
 alias xclipo='xclip -selection c -o'
 alias xclipjq='xclipo | jq . | xclipi'
-alias xclipfromjson='xclipo | jq fromjson | xclipi'
+alias xclipjs='xclipo | jq fromjson | xclipi'
