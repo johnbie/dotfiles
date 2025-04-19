aa6b21222086b6902a6199f69e9c6d1dc2c1f0b2
John Lee <johnqwerty.lee1@gmail.com>
2025-04-19 15:20:07 -0700

    Made xclipo compatible with the Windows new line

--- configs/clipboard/aliases.sh
+++ configs/clipboard/aliases.sh
@@ -1,4 +1,6 @@
 alias xclipi='xclip -selection c'
 alias xclipo='xclip -selection c -o'
+alias xclipo='xclip -selection c -o | tr -d "\r"'
+alias xclipo='(xclip -selection c -o | tr -d "\r"; echo)'
 alias xclipjq='xclipo | jq . | xclipi'
 alias xclipjs='xclipo | jq fromjson | xclipi'
