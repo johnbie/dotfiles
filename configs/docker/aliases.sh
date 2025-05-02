e2c1b15a39b2a8591b1758f944d927440d7bff09
John Lee <johnqwerty.lee1@gmail.com>
2025-05-02 08:36:45 -0700

    Moved the docker alias back to docker namespace

---
+++ configs/docker/aliases.sh
@@ -0,0 +1,2 @@
+alias docker-desktop='/opt/docker-desktop/bin/docker-desktop'
+
--- configs/kubernetes/aliases.sh
+++ configs/kubernetes/aliases.sh
@@ -1,3 +1,2 @@
-alias docker-desktop='/opt/docker-desktop/bin/docker-desktop'
 alias kube-watch-pod="watch -n 1 \"kubectl get pods -n \$(kubectl get ns | awk '{print \$1}' | fzf)\""
 
