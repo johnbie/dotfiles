319770ae944f787151c1ecddc1dc050574c5905e
John Lee <johnqwerty.lee1@gmail.com>
2025-05-02 08:41:39 -0700

    Added kubernetes aliases
    From here:
    - https://github.com/omerxx/dotfiles

--- configs/kubernetes/aliases.sh
+++ configs/kubernetes/aliases.sh
@@ -1,2 +1,15 @@
-alias kube-watch-pod="watch -n 1 \"kubectl get pods -n \$(kubectl get ns | awk '{print \$1}' | fzf)\""
+alias k="kubectl"
+alias ka="kubectl apply -f"
+alias kg="kubectl get"
+alias kd="kubectl describe"
+alias kdel="kubectl delete"
+alias kl="kubectl logs"
+alias kgpo="kubectl get pod"
+alias kgd="kubectl get deployments"
+alias kl="kubectl logs -f"
+alias ke="kubectl exec -it"
+# alias kc="kubectx
+# alias kns="kubens
+
+alias kw="watch -n 1 \"kubectl get pods -n \$(kubectl get ns | awk '{print \$1}' | fzf)\""
 
