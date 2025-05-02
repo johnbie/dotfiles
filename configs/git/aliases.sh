fd7084a1d05dca1b6a0d6615164a76436825305e
John Lee <johnqwerty.lee1@gmail.com>
2025-05-02 09:37:27 -0700

    Added more git aliases
    Based partially on https://github.com/omerxx/dotfiles

---
+++ configs/git/aliases.sh
@@ -0,0 +1,12 @@
+alias gc="git commit" # commit
+alias gca="git commit -a" # commit all
+alias ga="git add -p"
+alias gpush="git push"
+alias gpull="git pull"
+alias gs="git status"
+alias gd="git diff"
+alias gds="git diff --staged"
+alias gco="git checkout"
+alias gb="git branch"
+alias gba="git branch -a"
+
