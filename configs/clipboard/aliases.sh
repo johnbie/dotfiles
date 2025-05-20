113a79144f13f7f267f582478138b81f83ba70f9
John Lee <johnqwerty.lee1@gmail.com>
2025-05-19 17:11:45 -0700

    Alias changes

--- configs/clipboard/aliases.sh
+++ configs/clipboard/aliases.sh
@@ -1,4 +1,5 @@
-alias xclipi='xclip -selection c'
-alias xclipo='(xclip -selection c -o | tr -d "\r"; echo)'
+alias xclipi='xclip -selection c' # input
+alias xclipo='(xclip -selection c -o | tr -d "\r"; echo)' # output
+xclipm() { xclipo | "$@" | xclipi; }
 alias xclipjq='xclipo | jq . | xclipi'
 alias xclipjs='xclipo | jq fromjson | xclipi'
--- configs/docker/aliases.sh
+++ configs/docker/aliases.sh
@@ -1,2 +1,5 @@
 alias docker-desktop='/opt/docker-desktop/bin/docker-desktop'
 
+alias k='kubectl'
+alias kg='kubectl get'
+alias kwp="watch -n 1 \"kubectl get pods -n \$(kubectl get ns | awk '{print \$1}' | fzf)\""
