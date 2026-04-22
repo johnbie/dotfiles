750d42466096d9f409a838d70e5991a92da12783
John Lee <johnqwerty.lee1@gmail.com>
2026-04-21 19:26:51 -0700

    Add vim space as tab (comment out)x

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -40,6 +40,12 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l›', ':wincmd l<CR>')
 
+-- Spaces as tabs
+-- vim.cmd("set expandtab")
+-- vim.cmd("set tabstop=2")
+-- vim.cmd("set softtabstop=2")
+-- vim.cmd("set shiftwidth=2")
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
