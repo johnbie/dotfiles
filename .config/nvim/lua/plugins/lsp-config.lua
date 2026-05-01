9274a43b9960b69f4249917b68a61538a9f8a5fa
John Lee <johnqwerty.lee1@gmail.com>
2026-05-01 14:13:11 -0700

    Added lsp client

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -19,6 +19,7 @@ return {
         },
         config = function()
             require("mason-lspconfig").setup({
+                automatic_enable = false,
                 ensure_installed = {
                     "lua_ls",
                     "ts_ls",
@@ -29,12 +30,28 @@ return {
                     "yamlls",
                     "html",
                     "cssls",
-                    "gopls",
                     "dockerls",
                     "terraformls",
                     "lemminx",
                 }
             })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.lsp.enable("lua_ls")
+            vim.lsp.enable("ts_ls")
+            vim.lsp.enable("intelephense")
+            vim.lsp.enable("pyright")
+            vim.lsp.enable("bashls")
+            vim.lsp.enable("jsonls")
+            vim.lsp.enable("yamlls")
+            vim.lsp.enable("html")
+            vim.lsp.enable("cssls")
+            vim.lsp.enable("dockerls")
+            vim.lsp.enable("terraformls")
+            vim.lsp.enable("lemminx")
         end,
     }
 }
