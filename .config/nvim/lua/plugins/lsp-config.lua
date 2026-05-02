7818f476bac0a63c0452bfb46fd42ed94123b12a
John Lee <johnqwerty.lee1@gmail.com>
2026-05-01 23:02:20 -0700

    See virtual text

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -53,6 +53,8 @@ return {
             vim.lsp.enable("html")
             vim.lsp.enable("cssls")
 
+            vim.diagnostic.config({ virtual_text = true })
+
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
