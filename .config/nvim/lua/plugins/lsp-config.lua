49233d4ff68d8307a9d00dda9c87419e408413ea
John Lee <johnqwerty.lee1@gmail.com>
2026-05-01 22:53:06 -0700

    Add more keybindings for vim lsp

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -54,6 +54,8 @@ return {
             vim.lsp.enable("cssls")
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
         end,
     }
 }
